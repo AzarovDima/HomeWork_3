efc9d08a8d983c0b0fc11697eefea9cc6c91853f
AzarovDima <ada89885308140@gmail.com>
2022-12-24 15:07:19 +0300

    не могу устранить ошибку при введении некорректного формата

--- Prog_2_3D/Program.cs
+++ Prog_2_3D/Program.cs
@@ -2,7 +2,6 @@
 A (3,6,8); B (2,1,-7), -> 15.84
 A (7,-5, 0); B (1,-1,9) -> 11.53 */
 
-Console.Clear();
 int x1 = 0, y1 = 0, z1 = 0, x2 = 0, y2 = 0, z2 = 0; // A (x1, y1, z1), B (x2, y2, z2)!!!
 
 InitCoordinates(ref x1, ref y1, ref z1, ref x2, ref y2, ref z2);
@@ -38,4 +37,3 @@ static void PrintCoords(int x1, int y1, int z1, int x2, int y2, int z2)
     double resultRound = Math.Round(result, 2);
     Console.Write($"A ({x1},{y1},{z1}); B ({x2},{y2},{z2}) --> {resultRound}");
 }
-
